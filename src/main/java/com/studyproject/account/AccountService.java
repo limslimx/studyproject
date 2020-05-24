e18677cba04c3d9abcd2439d372ef7abd9bb8108
saemyunglim <saemyung2000@naver.com>
2020-05-25 00:52:42 +0900

    계정기능 - 회원가입시 비밀번호 암호화 및 테스트 완료

--- src/main/java/com/studyproject/account/AccountService.java
+++ src/main/java/com/studyproject/account/AccountService.java
@@ -4,6 +4,7 @@ import com.studyproject.domain.Account;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.validation.Valid;
@@ -14,6 +15,7 @@ public class AccountService {
 
     private final AccountRepository accountRepository;
     private final JavaMailSender javaMailSender;
+    private final PasswordEncoder passwordEncoder;
 
     public void processNewAccount(SignUpForm signUpForm) {
         Account newAccount = saveNewAccount(signUpForm);
@@ -25,7 +27,7 @@ public class AccountService {
         Account account = Account.builder()
                 .nickname(signUpForm.getNickname())
                 .email(signUpForm.getEmail())
-                .password(signUpForm.getPassword()) //TODO 패스워드 인코딩 해야함
+                .password(passwordEncoder.encode(signUpForm.getPassword()))
                 .studyCreatedByWeb(true)
                 .studyEnrollmentResultByWeb(true)
                 .studyUpdateByWeb(true)
